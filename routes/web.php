9fd9605a7476f5478b79199c3e0ccefa226397b5
kai.sato <studyabroad.kai@gmail.com>
2023-09-26 00:40:20 +0900

    fullclendar実装しました

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/calendar', function () {
+    return view('calendar');
+});
 
 Route::get('/', function () {
     return view('welcome');
