c9b4aa40f68faddc7a8ee0855e7c4b1ea9a1376d
kai.sato <studyabroad.kai@gmail.com>
2023-09-26 16:50:33 +0900

    前のコミットでバグが直っていなかった為再度修正。 Report　モデルに適当な処理を追加した。

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -1,11 +1,14 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    use HasFactory;
+    protected $fillable = ['content']; // データベースに書き込み可能なカラムを指定
+
+    protected $table = 'reports'; // モデルと対応するテーブル名を指定
+
+    // 他のモデルやリレーションシップがあればここに定義
 }
