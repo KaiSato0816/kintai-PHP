df3da667913d1fac29b388a91f8946024894f592
kai.sato <studyabroad.kai@gmail.com>
2023-10-31 00:43:13 +0900

    ユーザー情報変更画面編集

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
+            {{ __('勤務者情報変更フォーム') }}
         </h2>
     </x-slot>
 
